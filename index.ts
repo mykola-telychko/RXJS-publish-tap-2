015062cba3cf162bb90ab937daea8472bc7f82f3
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 13:45:31 +0300

    fin

--- index.ts
+++ index.ts
@@ -3,24 +3,25 @@ import { publish, tap } from 'rxjs/operators';
 
 // https://www.learnrxjs.io/learn-rxjs/operators/multicasting/publish
 
-// EXMPL - 1 - begin
+// EXMPL - 2 - begin
 //emit value every 1 second
 // Example 1: Connect observable after subscribers
-// const srcInterval = interval(1000); //do nothing until connect() is called
-
+//do nothing until connect() is called
 const exmpl_1 = publish()(
-  interval(1000).pipe(
-    //side effects will be executed once
-    tap((_) => console.log('Do Something!'))
-  )
+  interval(1000).pipe(tap((_) => console.log('Do Something!')))
 );
 
-const subscribe1 = exmpl_1.subscribe((v) => console.log(`Sbscrber 1: ${v}`));
-const subscribe2 = exmpl_1.subscribe((v) => console.log(`Sbscrber 2: ${v}`));
-const subscribe3 = exmpl_1.subscribe((v) => console.log(`Sbscrber 3: ${v}`));
+const subscribe1 = exmpl_1.subscribe((v) => console.log(`Sbscrber1: ${v}`));
+const subscribe2 = exmpl_1.subscribe((v) =>
+  console.log(`Sbscrber2: ${v - 10}`)
+);
+const subscribe3 = exmpl_1.subscribe((v) => console.log(`Sbscrber3: ${v}`));
+const subscribe4 = exmpl_1.subscribe((v) =>
+  console.log(`Sbscrber4: ${v - 20}`)
+);
 
 // call connect after 5 seconds, causing source to begin emitting items
 setTimeout(() => {
   exmpl_1.connect();
 }, 5000);
-// EXMPL - 1 - end
+// EXMPL - 2 - end
