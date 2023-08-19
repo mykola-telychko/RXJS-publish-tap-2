fc820a701f3ca9063c519c70ef0521c9b7ebac42
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 13:17:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-publish-tap-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-tcsygl)
---
+++ index.ts
@@ -0,0 +1,26 @@
+import { interval } from 'rxjs';
+import { publish, tap } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/multicasting/publish
+
+// EXMPL - 1 - begin
+//emit value every 1 second
+// Example 1: Connect observable after subscribers
+const srcInterval = interval(1000); //do nothing until connect() is called
+
+const exmpl_1 = publish()(
+  srcInterval.pipe(
+    //side effects will be executed once
+    tap((_) => console.log('Do Something!'))
+  )
+);
+
+const subscribe1 = exmpl_1.subscribe((v) => console.log(`Sbscrber 1: ${v}`));
+const subscribe2 = exmpl_1.subscribe((v) => console.log(`Sbscrber 2: ${v}`));
+const subscribe3 = exmpl_1.subscribe((v) => console.log(`Sbscrber 3: ${v}`));
+
+// call connect after 5 seconds, causing source to begin emitting items
+setTimeout(() => {
+  exmpl_1.connect();
+}, 5000);
+// EXMPL - 1 - end
